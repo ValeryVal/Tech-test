d09477dcf01f5f30c87fea4ae42a5467e5d6c096
Lavireall <75864114+Lavireall@users.noreply.github.com>
2022-09-08 20:43:49 +0300

    Imroved code
    Architecture changed
    Unittests added

--- Tech_test.py
+++ Tech_test.py
@@ -1,9 +1,9 @@
 import math
-
+import pytest
 
 class Figures:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
 
     def perimeter(self):
         pass
@@ -11,13 +11,19 @@ class Figures:
     def area(self):
         pass
 
+    def print_result(self):
+        return f"{self.name} perimeter: {self.perimeter()} {self.name} area: {self.area()}"
+
 
 class Square(Figures):
 
-    def __init__(self, top_right: list, side: int):
+    def __init__(self, parameters):
 
-        self.top_right = top_right
-        self.side = side
+        super().__init__("square")
+        self.top_right = (parameters[0], parameters[1])
+        self.side = parameters[2]
+        assert self.side >= 0, "side cannot be negative"
+
 
     def perimeter(self) -> float:
         return self.side*4
@@ -28,9 +34,10 @@ class Square(Figures):
 
 class Rectangle(Figures):
 
-    def __init__(self, top_right: list, bottom_left: list):
-        self.top_right = top_right
-        self.bottom_left = bottom_left
+    def __init__(self, parameters):
+        super().__init__("rectangle")
+        self.top_right = (parameters[0], parameters[1])
+        self.bottom_left = (parameters[2], parameters[3])
 
     def first_size(self):
         return abs(self.top_right[0]-self.bottom_left[0])
@@ -47,9 +54,12 @@ class Rectangle(Figures):
 
 class Circle(Figures):
 
-    def __init__(self, center: list, radius: int):
-        self.center = center
-        self.radius = radius
+    def __init__(self, parameters):
+        super().__init__("circle")
+        self.radius = parameters[2]
+        self.center = (parameters[0], parameters[1])
+
+
 
     def perimeter(self):
         return 2*math.pi*self.radius
@@ -58,8 +68,28 @@ class Circle(Figures):
         return math.pi*self.radius**2
 
 
+def test_square():
+    square = Square([2, 3, 4])
+    assert square.perimeter() == 16
+    assert square.area() == 16
+
+def test_result():
+    square = Square([2, 3, 4])
+    assert square.print_result() == "square perimeter: 16 square area: 16"
+
+def test_rectangle_validation():
+    rectangle = Rectangle([2, 3, 2, 3])
+    assert rectangle.print_result() == "rectangle perimeter: 0 rectangle area: 0"
+
+def test_square_validation():
+
+    with pytest.raises(Exception):
+        square = Square([2, 3, -1])
+
+
+
 if __name__ == "__main__":
-    figures = ["square", "rectangle", "circle"]
+    figures = {"square": Square, "rectangle": Rectangle, "circle": Circle}
     figure_name = input("Write your figure:")
 
     if figure_name.lower() in figures:
@@ -71,16 +101,17 @@ if __name__ == "__main__":
     else:
         print(f"Write correct figure from the list: {figures}")
 
-    if figure_name.lower() == "square":
-        square = Square(list((end_param[0], end_param[1])), side=end_param[2])
-        print("Square perimeter:", square.perimeter(),
-              "Square area:", square.area())
-    if figure_name.lower() == "rectangle":
-        rectangle = Rectangle(list((end_param[0], end_param[1])), list((end_param[2], end_param[3])))
-        print("Rectangle perimeter:", rectangle.perimeter(),
-              "Rectangle area:", rectangle.area())
-    if figure_name.lower() == "circle":
-        circle = Circle(list((end_param[0], end_param[1])), end_param[2])
-        print("circle perimeter:", circle.perimeter(),
-              "circle area:", circle.area())
+    figure = None
+
+    for key in figures:
+        if figure_name.lower() == key:
+            try:
+                figure = figures[key](end_param)
+            except Exception as e:
+                print("error:", str(e))
+
+
+
+    if figure:
+        print(figure.print_result())
 
